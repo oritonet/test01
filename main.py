7ec18e589aab30b9789cef81a9ce42ba893afc03
oritonet <o47638708@gmail.com>
2025-07-31 23:15:45 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,8 @@
 import flet as ft
 
-async def main(page: ft.Page):
-    page.title = "Flet counter example"
-    txt = ft.TextField(value="0", text_align=ft.TextAlign.RIGHT, width=100)
+def main(page: ft.Page):
+    page.title = "Hello Flet Web"
+    page.add(ft.Text("Hello from Flet Web!"))
 
-    def minus(e):
-        txt.value = str(int(txt.value) - 1)
-        page.update()
-
-    def plus(e):
-        txt.value = str(int(txt.value) + 1)
-        page.update()
-
-    page.add(
-        ft.Row(
-            [
-                ft.IconButton(ft.icons.REMOVE, on_click=minus),
-                txt,
-                ft.IconButton(ft.icons.ADD, on_click=plus),
-            ],
-            alignment=ft.MainAxisAlignment.CENTER,
-        )
-    )
-
-ft.app(target=main)
+if __name__ == "__main__":
+    ft.app(target=main)
