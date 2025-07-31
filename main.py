1d68c522d822edff9143174cc1b3e58a7d3b93b0
oritonet <o47638708@gmail.com>
2025-07-31 22:52:45 +0900

    main.py

--- main.py
+++ main.py
@@ -1,9 +1,26 @@
-from flet.fastapi import FastAPI
 import flet as ft
 
 def main(page: ft.Page):
-    page.title = "Flet Render Web App"
-    txt = ft.Text("Hello from Flet on Render!")
-    page.add(txt)
+    page.title = "Flet counter example"
+    txt = ft.TextField(value="0", text_align=ft.TextAlign.RIGHT, width=100)
 
-app = FastAPI(target=main)
+    def minus(e):
+        txt.value = str(int(txt.value) - 1)
+        page.update()
+
+    def plus(e):
+        txt.value = str(int(txt.value) + 1)
+        page.update()
+
+    page.add(
+        ft.Row(
+            [
+                ft.IconButton(ft.icons.REMOVE, on_click=minus),
+                txt,
+                ft.IconButton(ft.icons.ADD, on_click=plus),
+            ],
+            alignment=ft.MainAxisAlignment.CENTER,
+        )
+    )
+
+ft.app(target=main)
