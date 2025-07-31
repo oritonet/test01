ea9c4281613e287201102939e9ac6a01fe93e51a
oritonet <o47638708@gmail.com>
2025-08-01 00:23:59 +0900

    main.py の更新

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ import flet as ft
 
 def main (page: ft.Page):
     page.title = "My App
-    page.add(ft. Text("Hello from Flet on Render!") )
+    page.add(ft. Text("Hello from Flet on Render!!!!") )
 if __name__ == "__main__":             
     ft.app(target=main,port=int(os.environ.get("PORT",8550)),host="0.0.0.0")
