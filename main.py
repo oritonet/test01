ad95788a9b616e8536df3edd0de936aec5d81504
oritonet <o47638708@gmail.com>
2025-08-01 00:14:29 +0900

    main.py の更新

--- main.py
+++ main.py
@@ -1,15 +1,9 @@
 import os
 import flet as ft
 
-async def main(page: ft.Page):
-    page.title = "Happy!!"
-    page.vertical_alignment = ft.MainAxisAlignment.CENTER
-    txt = ft.Text(value="Hello Render!", size=30)
-    page.add(txt)
-
-# ポートとホストを Render 向けに設定
-ft.app_async(
-    target=main,
-    port=int(os.environ.get("PORT", 8000)),
-    host="0.0.0.0"
-)
+def main (page: ft.Page):
+    page.title = "My App
+    page.add(ft. Text("Hello from Flet on Render!") )
+if -_name__ =="
+    ft. app(target=main, port=int(os.environ.get ("PORT"
+8550)), host="0.0.0.0")
