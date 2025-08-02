08fbcae004268e5c6eed4b97c9402a61f1796180
oritonet <o47638708@gmail.com>
2025-08-02 09:19:22 +0900

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import flet as ft
 
 def main(page: ft.Page):
     page.title = "Hello Flet Web"
-    page.add(ft.Text("Hello from Flet Web!"))
+    page.add(ft.Text("Hello from Flet Web!!!!"))
 
 if __name__ == "__main__":
     ft.app(target=main, port=int(os.environ.get("PORT", 8550)), host="0.0.0.0")
